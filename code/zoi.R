da732b19443568a2571b7328c3f505c2189e8448
Ariel <ariel.fuentesdi@usach.cl>
2021-12-23 17:53:22 -0300

    rm unnecesary code

--- code/zoi.R
+++ code/zoi.R
@@ -1,17 +1,15 @@
 library(sf)
 library(tmap)
 # plot(zones[,1])
+
 rts_shp <- st_read("data/Shapes 06Jul2019.shp") %>%
   filter(UN == 2)
 # plot(rts_shp[,1])
 
+#polygons of interest ----
 zoi <- st_filter(x = zones, y = rts_shp, .predicate = st_intersects) 
 
-tm_shape(zoi) +
-  tm_polygons(col = "orange") +
-  tm_shape(zones) +
-  tm_polygons(col = "gray")
-
+#create map
 tm_shape(zones) +
   tm_grid(col = "white", lwd = 2, labels.size = .8) +
   tm_polygons(col = "gray") +
