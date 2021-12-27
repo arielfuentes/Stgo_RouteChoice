aa31b723eb4c3b1f1711e256d712a63932501726
Ariel <ariel.fuentesdi@usach.cl>
2021-12-27 18:21:38 -0300

    init

---
+++ code/EDA.R
@@ -0,0 +1,4 @@
+ggplot(vjs_pma, aes(periodomediodeviaje, tviaje)) + geom_boxplot()
+ggplot(vjs_pma, aes(periodomediodeviaje, tesp)) + geom_boxplot()
+ggplot(vjs_pma, aes(periodomediodeviaje, tb2)) + geom_boxplot()
+ggplot(vjs_pma, aes(periodomediodeviaje, tcam)) + geom_boxplot()
