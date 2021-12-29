2cf4623cecfeaf1812a56d67e191480e09b0499d
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2021-12-29 01:32:40 -0300

    change x-axis ggplot

--- code/EDA.R
+++ code/EDA.R
@@ -1,4 +1,4 @@
-ggplot(vjs_pma, aes(periodomediodeviaje, tviaje)) + geom_boxplot()
-ggplot(vjs_pma, aes(periodomediodeviaje, tesp)) + geom_boxplot()
-ggplot(vjs_pma, aes(periodomediodeviaje, tb2)) + geom_boxplot()
-ggplot(vjs_pma, aes(periodomediodeviaje, tcam)) + geom_boxplot()
+ggplot(vjs_pma, aes(as.factor(netapa), tviaje)) + geom_boxplot()
+ggplot(vjs_pma, aes(as.factor(netapa), tesp)) + geom_boxplot()
+ggplot(vjs_pma, aes(as.factor(netapa), tb2)) + geom_boxplot()
+ggplot(vjs_pma, aes(as.factor(netapa), tcam)) + geom_boxplot()
